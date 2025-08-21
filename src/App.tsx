553195a887d3ba2ef79babdbbcad6f91b9893ddd
Shikhar Vishwakarma <shikharvishwa123@gmail.com>
2025-08-21 23:40:59 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/components/pages/Index";
-import Login from "@/components/pages/login";
+import Login from "@/components/pages/Login";
 import Register from "@/components/pages/Register";
 import NotFound from "@/components/pages/NotFound";
 
